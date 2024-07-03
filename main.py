151d75c52dad85c9843e840f39df163ac8f962a6
Socitt <shawn.jr@faulkingham.org>
2024-07-03 08:18:12 -0700

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen.setup(1300,600)
 screen.setworldcoordinates(-180,-90,180,90)
 screen.bgpic("\git\smallprojects\isstracker\map1.gif")
 
-#api call
+#api call 
 url = "http://api.open-notify.org/iss-now.json"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
